4550a428353bf78f2782e37d8238e8f808cd1656
JlexaSS <jowe.jlexa@mail.ru>
2022-05-12 15:05:42 +0300

    refactor: replace constructor with annotation

--- src/main/java/com/example/JustLearning/domain/VisitedSights.java
+++ src/main/java/com/example/JustLearning/domain/VisitedSights.java
@@ -1,6 +1,7 @@
 package com.example.JustLearning.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -13,6 +14,7 @@ import javax.persistence.ManyToOne;
 
 @Getter
 @Setter
+@NoArgsConstructor
 @Entity
 public class VisitedSights {
     @Id
@@ -29,8 +31,6 @@ public class VisitedSights {
 
     private Float rating;
     private Boolean visited;
-
-    public VisitedSights(){}
 
     public VisitedSights(Sights sight, User user, Float rating){
         this.sight = sight;
