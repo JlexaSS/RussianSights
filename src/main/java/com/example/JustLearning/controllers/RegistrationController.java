4b85cfc659ebc0dedb957250f5c71b9f87a40f36
JlexaSS <jowe.jlexa@mail.ru>
2022-05-13 16:34:22 +0300

    refactor move registration function to service

--- src/main/java/com/example/JustLearning/controllers/RegistrationController.java
+++ src/main/java/com/example/JustLearning/controllers/RegistrationController.java
@@ -1,10 +1,7 @@
 package com.example.JustLearning.controllers;
 
-import com.example.JustLearning.domain.Role;
 import com.example.JustLearning.domain.User;
-import com.example.JustLearning.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import com.example.JustLearning.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,18 +9,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Collections;
-import java.util.UUID;
 
 @Controller
 public class RegistrationController {
-    @Autowired
-    UserRepository userRepository;
-
-    @Value("${upload.path}")
-    private String uploadPath;
+    private final UserService userService;
+    public RegistrationController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/registration")
     public String registration(){
@@ -32,30 +25,9 @@ public class RegistrationController {
 
     @PostMapping("/registration")
     public String addUser(User user, @RequestParam("file") MultipartFile file, Model model) throws IOException {
-        User userFromDb = userRepository.findByUsername(user.getUsername());
-        if (userFromDb != null){
-            model.addAttribute("message", "User exists!");
-            return "registration";
-        }
-        if(file.getOriginalFilename().isEmpty()){
-            user.setImgProfile("68e2b43f-0a8f-4ed6-97a7-9fde4314e703.profileimage.png");
-        } else{
-            File uploadDir = new File(uploadPath);
-
-            if(!uploadDir.exists()){
-                uploadDir.mkdir();
-            }
-
-            String uuidFile = UUID.randomUUID().toString();
-            String resultFilename = uuidFile + "." + file.getOriginalFilename();
-
-            file.transferTo(new File(uploadPath + "/" + resultFilename));
-
-            user.setImgProfile(resultFilename);
-        }
-        user.setActive(true);
-        user.setRoles(Collections.singleton(Role.USER));
-        userRepository.save(user);
+        if (userService.addNewUser(user, file, model)) return "registration";
         return "redirect:/";
     }
+
+
 }
--- src/main/java/com/example/JustLearning/service/UserService.java
+++ src/main/java/com/example/JustLearning/service/UserService.java
@@ -3,22 +3,32 @@ package com.example.JustLearning.service;
 import com.example.JustLearning.domain.Role;
 import com.example.JustLearning.domain.User;
 import com.example.JustLearning.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Map;
 import java.util.Set;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
     private final UserRepository userRepository;
+    private final String uploadPath;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, @Value("${upload.path}") String uploadPath) {
         this.userRepository = userRepository;
+        this.uploadPath = uploadPath;
     }
 
     @Override
@@ -37,5 +47,33 @@ public class UserService implements UserDetailsService {
                 user.getRoles().add(Role.valueOf(key));
 
         userRepository.save(user);
+    }
+
+    public boolean addNewUser(User user, @RequestParam("file") MultipartFile file, Model model) throws IOException {
+        User userFromDb = userRepository.findByUsername(user.getUsername());
+        if (userFromDb != null){
+            model.addAttribute("message", "User exists!");
+            return true;
+        }
+        if(file.getOriginalFilename().isEmpty()){
+            user.setImgProfile("68e2b43f-0a8f-4ed6-97a7-9fde4314e703.profileimage.png");
+        } else{
+            File uploadDir = new File(uploadPath);
+
+            if(!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+
+            String uuidFile = UUID.randomUUID().toString();
+            String resultFilename = uuidFile + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+
+            user.setImgProfile(resultFilename);
+        }
+        user.setActive(true);
+        user.setRoles(Collections.singleton(Role.USER));
+        userRepository.save(user);
+        return false;
     }
 }
